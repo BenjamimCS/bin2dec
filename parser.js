35142a40e667987072b67cb29fee4b0a2d89e620
BenjamimCS <idemalr892@gmail.com>
2022-02-28 04:39:18 -0300

    Making the code clear

--- parser.js
+++ parser.js
@@ -1,3 +1,5 @@
+import {isBin,alertBoxAnim,showResult} from './utilities.js';
+
 const alertBoxArea = document.getElementById('alertBox-area');
 const alertBoxTextArea = document.getElementById('alertBox-text');
 const binString = document.getElementById('binInput');
@@ -5,53 +7,46 @@ const toParseBtn = document.getElementById('toParseBtn');
 const clearResult = document.getElementById('cleanBtn');
 const resultSpan = document.getElementById('result-span');
 
-const alertBoxStndrtMarginRight = '-290px';
+const alertBoxStndrtMargin = '-290px';
 
 binString
-.addEventListener(
-'keydown',
-    function(k){                                
+    .addEventListener(
+    'keydown',
+    (k) =>{                                
         if(k.keyCode === 13){
             bin2Dec();
-        }                           
-});
+        }
+    }
+);
 toParseBtn
     .addEventListener('click',bin2Dec);
 clearResult
     .addEventListener('click',toClear);
 
-const isBin = ar => 
-              ar.filter(a => 
-              a === 1 || a === 0).length === ar.length;
-
-const alertBoxAnim = () => alertBoxArea.style.marginRight = alertBoxStndrtMarginRight;            
-
-function bin2Dec(){    
+function bin2Dec(){
     let binArr = Array.from(binString.value,(n) => Number(n));
 
     let expo = binArr.length;
-    let result = 0;
+    let acc = 0;
 
     if(binString.value.length === 0 || !isBin(binArr)){
         alertBoxTextArea.textContent = 'Empty or Non-Binary Value';
         alertBoxArea.style.marginRight = '0px';
 
-        setTimeout(alertBoxAnim,1500);
+        alertBoxAnim(alertBoxArea,2,alertBoxStndrtMargin,1500)
         
         binString.focus();
     } else{
         
-        for (numbers of binArr){
-            result += (2 ** --expo) * numbers;
+        for (let numbers of binArr){
+            acc += (2 ** --expo) * numbers;
         }
 
-        resultSpan.innerHTML = result;
-        resultSpan.style.marginTop = '0';
-        resultSpan.style.opacity = '1';
-
+        resultSpan.innerHTML = acc;
+        showResult(resultSpan);
     }
-
-    setTimeout(alertBoxAnim,1500);
+    
+    alertBoxAnim(alertBoxArea,2,alertBoxStndrtMargin,1500);
 
     binString.focus();
 }
@@ -59,19 +54,18 @@ function bin2Dec(){
 function toClear(){
     if(resultSpan.textContent === ''){
         alertBoxTextArea.textContent = 'Nothing To Clear';
-    alertBoxArea.style.marginRight = '0px';
+        alertBoxArea.style.marginRight = '0px';
 
-        setTimeout(alertBoxAnim,1500);
+        alertBoxAnim(alertBoxArea,2,alertBoxStndrtMargin,1500);
 
         binString.focus();
     } else{
         binString.value = '';
         
-        resultSpan.style.opacity = '0';
-        resultSpan.style.marginTop = '';
+        showResult(resultSpan);
         resultSpan.textContent = '';        
 
-        setTimeout(alertBoxAnim,1500);
+        alertBoxAnim(alertBoxArea,2,alertBoxStndrtMargin,1500);
         
         binString.focus();
     }
