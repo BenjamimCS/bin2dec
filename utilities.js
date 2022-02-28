eb692fc45b54a5b7bdd739b65979a5c4d578fee1
BenjamimCS <83431838+BenjamimCS@users.noreply.github.com>
2022-02-28 04:39:18 -0300

    Making the code clear

---
+++ utilities.js
@@ -0,0 +1,34 @@
+// Utilities File
+
+
+export const
+    isBin = (ar) => 
+    ar.filter((a) => 
+    a === 1 || a === 0).length === ar.length,
+
+    alertBoxAnim = (obj,margin,value,gap = 0) => {
+
+        setTimeout(() => {
+            if(typeof margin === 'number')
+                switch(margin){
+                    case 1:
+                        obj.style.marginTop = value;
+                        break;
+                    case 2:
+                        obj.style.marginRight = value;
+                        break;
+                    case 3:
+                        obj.style.marginBottom = value;
+                        break;
+                    case 4:
+                        obj.style.marginLeft = value;
+                        break;        
+                }
+            
+        },gap);
+    },
+
+    showResult = (obj) => {
+        obj.style.marginTop = '0';
+        obj.style.opacity = '1';
+    };
